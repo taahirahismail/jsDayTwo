ff64a4918cec951419bf96ac9bcdae86f285bf82
taahirahismail <taahirah.i@gmail.com>
2023-05-16 16:04:32 +0200

    exercise done?

---
+++ script/code.js
@@ -0,0 +1,7 @@
+const subjects = ['Python', 'C++', 'VB.net', 'JavaScript', 'Assembly'];
+
+let lastElement = subjects.pop();
+console.log(lastElement);
+
+let midElement = subjects[2];
+console.log(midElement);
